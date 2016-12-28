bcec345e8a225a47acda262d6f06561dbdce2ff4
John Black <john.f.black@icloud.com>
2016-12-28 13:58:47 -0500

    started jquery file

---
+++ js/image.js
@@ -0,0 +1,17 @@
+$(function() {
+
+console.log("Document Ready");
+
+$(".image_text").hide(); 
+    
+$(".one").mouseover(function() {
+   $(".one").hide();
+});
+    
+    
+    
+    
+    
+    
+    
+});
