95419f8fb330312cd3bf03d58aca61ced4b11ea1
John Black <john.f.black@icloud.com>
2018-03-18 00:23:20 -0400

    added LA, IA, and NW

--- script.js
+++ script.js
@@ -34,20 +34,50 @@ $(document).ready(function(){
       let mon3 = numberWithCommas(mon2);
       console.log(`Months: ${mon3}`);
 
+      let cfok = $('.cfok').val();
+      let cfok2 = parseInt(cfok);
+      let cfok3 = numberWithCommas(cfok2);
+      console.log(`401k: $${cfok3}`);
+
+      let cst = $('.cst').val();
+      let cst2 = parseInt(cst);
+      let cst3 = numberWithCommas(cst2);
+      console.log(`Stock: $${cst3}`);
+
+      let fok = $('.fok').val();
+      let fok2 = parseInt(fok);
+      let fok3 = numberWithCommas(fok2);
+      console.log(`401k Monthly: $${fok3}`);
+
+      let st = $('.st').val();
+      let st2 = parseInt(st);
+      let st3 = numberWithCommas(st2);
+      console.log(`Stock Monthly: $${st3}`);
+
       let mr = (pay2 * 24)/12;
       console.log(`Monthly Pay: $${mr}`);
 
+      let tNetW = cfok2 + cst2 + sav2 + (pay2 * mon2) - (bil2 * mon2) + (fok2 * mon2) + (st2 * mon2);
+      console.log(`Total Net Worth: $${numberWithCommas(tNetW)}`);
+
+      //ms + cst + fok + st
+
       let ms = sav2;
+      let me = sav2 + cst2;
+      let mi = cfok2;
+      let mnw = me + mi;
       let x = 0;
-      let html = '<table class="table table-bordered"><tr><th>Month</th><th>Savings</th></tr>';
+      let html = '<table class="table table-bordered"><tr><th>Month</th><th>Savings</th><th>Liquid Assets</th><th>Illiquid Assets</th><th>Net Worth</th></tr>';
       while (x < mon2) {
         ms += mr - bil2;
-        console.log(`Month ${x} Savings: $${numberWithCommas(Math.round(ms))}`);
-        html += `<tr><td>${x}</td><td>$${numberWithCommas(Math.round(ms))}</td></tr>`;
+        me += (mr - bil2) + st2;
+        mi += fok2;
+        mnw += (mr - bil2) + st2 + fok2;
+        html += `<tr><td>${x}</td><td>$${numberWithCommas(Math.round(ms))}</td><td>$${numberWithCommas(Math.round(me))}</td><td>$${numberWithCommas(Math.round(mi))}</td><td>$${numberWithCommas(Math.round(mnw))}</td></tr>`;
         x++;
       }
       if (x !=0) {
-          $(".table").append(`<h3>Monthly Contribution: $${numberWithCommas(Math.round(mr - bil2))}</h3>` + html + '</table>');
+          $(".table").append(`<h3>Monthly Contribution: $${numberWithCommas(Math.round(mr - bil2))}</h3><h3>Total Net Worth: $${numberWithCommas(Math.round(mnw))}</h3>` + html + '</table>');
       }
 
       });
